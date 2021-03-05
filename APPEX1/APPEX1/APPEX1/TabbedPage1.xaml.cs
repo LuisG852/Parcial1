5e8d1510804b9f1f5015bf0bb31d20a831f5f805
LuisG852 <79346137+LuisG852@users.noreply.github.com>
2021-03-04 23:27:06 -0600

    Parcial1
    Parcial1

---
+++ APPEX1/APPEX1/APPEX1/TabbedPage1.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace APPEX1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TabbedPage1 : TabbedPage   
+    {
+        
+        public TabbedPage1()
+        {
+            InitializeComponent();
+        }
+        public void B1(object sender, EventArgs e)
+        {
+            var lol = int.Parse(en1.Text);
+
+            int res, dia, fecha, A;
+
+            string Total = "", namemes = "";
+            A = (lol - 365);
+            res = A;
+            dia = res / 100;
+            fecha = res % 100;
+            Total = "Usted nacio el dia " + dia + " y  el  mes " + fecha + "(" + namemes + ")";
+
+            DisplayAlert("La Respueta es:",
+                Total,
+                "");
+        }
+        private void B2(object sender, EventArgs e)
+        {
+
+            var sis = int.Parse(en2.Text);
+            int x, resul;
+            x = sis / 10;
+            resul = x - 4;
+            string Total = "";
+            Total = "El numero que estabas pensado es : " + resul;
+            DisplayAlert("Resultado", Total, "");
+        }
+    }
+}
